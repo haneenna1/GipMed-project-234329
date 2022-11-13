835f1e579292a7bad2dca46fd00ab84eddc6b645
Haneen Naaran <haneen.na.110@gmail.com>
2022-11-14 00:25:58 +0200

    defined new function: get_loaders

--- dataset.py
+++ dataset.py
@@ -14,4 +14,24 @@ class ThumbnailsDataset(Dataset):
 
     def __getitem__(self, index):
         #TODO: AMIR
+        pass
+        
 
+#TODO: implement this function  
+
+
+def get_loaders(train_img_dir, train_mask_dir, val_img_dir, val_maks_dir, batch_size, train_transforms, val_transforms, num_workers, pin_memory):
+    """returns train and validation dataloaders
+
+    Args:
+        train_img_dir: path to the directory of the training images
+        train_mask_dir: path to the directory of the training masks
+        val_img_dir: path to the directory of the validation images
+        val_maks_dir: path to the directory of the validation masks
+        batch_size: batch size of the dataloaders
+        train_transforms: a sequence of transformations to apply on the training set
+        val_transforms: a sequence of transformations to apply on the validation set
+        num_workers: num workers for the data loading 
+        pin_memory 
+    """
+    pass
