90512333209072537ad585b8685b743b6f264f6f
Haneen Naaran <haneen.na.110@gmail.com>
2022-11-14 16:12:40 +0200

    modifications on the train class

--- train.py
+++ train.py
@@ -7,67 +7,53 @@ from model import Unet
 from dataset import ThumbnailsDataset, get_loaders    
 from utils import save_checkpoint, load_checkpoint
 
-LEARNING_RATE = 1e-4
-NUM_EPOCHS = 3
-BATCH_SIZE = 16
-PIN_MEMPRY = True
-NUM_WORKERS = 2
+class Train:
+    def __init__(self, model, optimizer, loss_fn, img_dir, mask_dir,
+                 train_transform, val_transform, hyper_paramas, load_model):
+        
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.model = model
+        if load_model:
+            load_checkpoint(self.model)
+        
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=learningRate)
+        self.loss_fn = loss_fn
+        self.hyper_params = hyper_paramas
+        self.dataset = ThumbnailsDataset(ImageDir, maskDir)
+        self.train_loader, self.val_loader = get_loaders(img_dir, mask_dir, batch_size, 
+                                                         train_transforms, val_transforms, num_workers, pin_memory)
+        
+    def train_epoch(self):
+        loop = tqdm(self.train_loader)
 
-# Setting the device for the Training
-DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+        for batch_index, (data, targets) in enumerate(loop): # each iteration is one epoch 
+            data = data.to(self.device)
+            targets = labels.to(self.device)   # check if need to unsqueeze
 
-
-def train_epoch(model, loader, optimizer, scaler, loss_fn):
-    loop = tqdm(loader)
-
-    for batch_index, (data, labels) in enumerate(loop): # each iteration is one epoch 
-        data = data.to(device = DEVICE)
-        labels = labels.to(device = DEVICE)   # check if need to unsqueeze
-
-        #forward
-        with torch.cuda.amp.autocast(): 
+            #forward
             predictions = model(data)
             loss = loss_fn(predictions, targets)
 
-        #backward
-        optimizer.zero_grad()
-        scaler.scale(loss).backward()
-        scaler.step(optimizer)
-        scaler.update()
+            #backward
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-        #update tqdm 
-        loop.set_postfix(loss = loss.item())
-        
-def main(): 
-    train_transform = []
-    val_transforms = []
-    model = Unet(in_channels=3, out_channels=2).to(DEVICE)
-    loss_fn = nn.BCEWithLogitsLoss()
-    optimizer = optim.Adam(model.parameters(), lr = LEARNING_RATE)
-    scaler = torch.cuda.amp.GradScaler()
-    train_loader, val_loader = get_loaders(
-        TRAIN_IMG_DIR, 
-        TRAIN_MASK_DIR,
-        VAL_IMG_DIR, 
-        VAL_MAKS_DIR,
-        BATCH_SIZE, 
-        train_transforms, 
-        val_transforms, 
-        NUM_WORKERS,
-        PIN_MEMPRY
-    )
-
-    for epoch in range(NUM_EPOCHS):
-        train_epoch(model, train_loader, optimizer, scaler, loss_fn)
-
-        # save model every two epochs
-        if(epoch %2 == 0 ): 
-            checkpoint = {model_state: model.state_dict(), optimizer: optimizer.state_dict()} 
-            save_checkpoint(checkpoint, file_name = f'unet_checkpoint_epoch{epoch}')
-        
-        # check accuuracy
-
-        # print some examples to a file 
-        
-if __name__=="__main__": 
-    main()
+            #update tqdm 
+            loop.set_postfix(loss = loss.item())
+            
+    def train(self): 
+        for epoch in range(self.hyper_params.num_epochs):
+            self.train_epoch()
+            
+            #after each epoch, check accuracy on validation set 
+            check_accuracy(val_loader, model, device=DEVICE)
+            
+            # print some examples to a folder
+            save_predictions_as_imgs(
+                val_loader, model, folder="saved_images/", device=DEVICE
+            )
+            # save model every two epochs
+            if(epoch %2 == 0 ): 
+                checkpoint = {model: model.state_dict(), optimizer: optimizer.state_dict()} 
+                save_checkpoint(checkpoint)
