33fcb745c49e92383756552185dc62676a866ca8
Haneen Naaran <haneen.na.110@gmail.com>
2022-11-14 16:09:43 +0200

    added main file for running the model

---
+++ main.py
@@ -0,0 +1,29 @@
+
+from train import Train
+
+# Setting the device for the Training
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+LEARNING_RATE = 1e-4
+NUM_EPOCHS = 3
+BATCH_SIZE = 16
+PIN_MEMPRY = True
+NUM_WORKERS = 2 # <= cpus
+
+def main():
+    model = Unet(in_channels=3, out_channels=2).to(DEVICE)
+    optimizer = optim.Adam(model.parameters(), lr = LEARNING_RATE)
+    loss_fn = nn.BCEWithLogitsLoss()
+    img_dir = ""
+    maks_dir = ""
+    train_transform = []
+    val_transforms = []
+    hyper_paramas = {lr:LEARNING_RATE, num_epochs:NUM_EPOCHS, 
+                       batch_size:BATCH_SIZE, pin_memory:PIN_MEMPRY, num_workers:NUM_WORKERS }
+    
+    trainer = Train(model, optimizer, loss_fn, img_dir, mask_dir, train_transform, val_transform, hyper_paramas)
+    trainer.train()
+    
+
+if __name__ == "__main__": 
+    main()
