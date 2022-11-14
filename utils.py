9599c9945c0cafc29919f467e9732fd03513f5e2
Haneen Naaran <haneen.na.110@gmail.com>
2022-11-14 16:05:38 +0200

    added utils file

--- utils.py
+++ utils.py
@@ -1,8 +1,44 @@
-def save_checkpoint(checkpoint, file_name = "model_checkpoint.pth.tar") :
-    print("==> saving check point ")
-    torch.save(checkpoint, file_name)
+def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
+    print("=> Saving checkpoint")
+    torch.save(state, filename)
 
+def load_checkpoint(model, filename="my_checkpoint.pth.tar"):
+    print("=> Loading checkpoint")
+    check_point = torch.load(filename)
+    model.load_state_dict(checkpoint["model"])
 
-#TODO
-def load_checkpoint(file_name = "model_checkpoint.pth.tar"):
-    pass
+def check_accuracy(loader, model, device="cuda"):
+    num_correct = 0
+    num_pixels = 0
+    dice_score = 0
+    model.eval() # set the model to be in eval mode not train mode, for parts that behave differently in train/val
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device)
+            y = y.to(device).unsqueeze(1)
+            preds = torch.sigmoid(model(x))
+            preds = (preds > 0.5).float()
+            num_correct += (preds == y).sum()
+            num_pixels += torch.numel(preds)
+            dice_score += (2 * (preds * y).sum()) / (
+                (preds + y).sum() + 1e-8
+            )
+
+    print(
+        f"Got {num_correct}/{num_pixels} with acc {num_correct/num_pixels*100:.2f}"
+    )
+    print(f"Dice score: {dice_score/len(loader)}")
+    model.train() # set the model back to the training mode 
+
+def save_predictions_as_imgs(loader, model, folder="saved_images/", device="cuda"):
+    model.eval() 
+    for idx, (x, y) in enumerate(loader):
+        x = x.to(device=device)
+        with torch.no_grad():
+            preds = torch.sigmoid(model(x))
+            preds = (preds > 0.5).float()
+        torchvision.utils.save_image(preds, f"{folder}/pred_{idx}.png")
+        torchvision.utils.save_image(y.unsqueeze(1), f"{folder}{idx}.png")
+
+    model.train() 
