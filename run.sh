81cd0b62875bf4e44bfd67656cca7be48953a814
Haneen Naaran <haneen.na.110@gmail.com>
2022-11-22 23:55:56 +0200

    fixed run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 #SBATCH -p gipmed
+#SBATCH -N 1
+#SBATCH -n 1
 #SBATCH --gres=gpu:2
-#SBATCH -c 4      # cores requested
-#SBATCH -o ./sbatch_out/%j.out
+#SBATCH -c 4
+#SBATCH -o sbatch_out
+#SBATCH -e sbatch_err
+
 module load /home/haneenna/miniconda3/envs/GipMed/bin/python
-python /home/haneenna/GipMed-project-234329/main.py
+/home/haneenna/miniconda3/envs/GipMed/bin/python /home/haneenna/GipMed-project-234329/main.py
