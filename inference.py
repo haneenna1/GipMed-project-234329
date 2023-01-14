ac60751f64480774df8e917655e4ae24fcd8bfb4
Amir Bishara <amir.bishara@gipdeep.cs.technion.ac.il>
2023-01-14 11:59:44 +0200

    fix adding visulaizing for the three classes segmentation [git issue]

--- inference.py
+++ inference.py
@@ -64,15 +64,9 @@ class Inferer:
                 inferred_mask = inferred_mask.cpu()
                 inferred_mask_numpy = PIL.Image.fromarray(np.array(inferred_mask, dtype = np.uint8).reshape(inferred_mask.shape[2],inferred_mask.shape[3]))
                 inferred_mask_numpy.save(inferred_mask_path_png)
-
-                # save_012_mask_as_img(inferred_mask.squeeze(1), inferred_mask_path_jpg)
-
-                # stam = np.array(PIL.Image.open(inferred_mask_path).convert("L"), dtype=np.float32)
-                # print(np.count_nonzero(stam == 2))
-                # print(stam.shape)
                 
                 if visulaize:
-                    # self.visulaize_shade(image_path, mask_path, inferred_mask_path, index)
+                    # self.visulaize_shade(image_path, mask_path, inferred_mask_path_png, index)
                     self.visulaize_sharp(image_path, mask_path, inferred_mask_path_png, index)
                
                 self.calculate_metrics(per_pixel_pred_scores,mask.unsqueeze(1))
@@ -86,56 +80,38 @@ class Inferer:
     # Getting an overlayed image with a shade of colors [Green --- Blue]
     # the sharp Blue indicates the original mask, and the sharp Green 
     # indicates the inferred mask. the light blue regions would indicates both
-    def visulaize_shade(self, image_path, mask_path, inferred_mask_path, index)->None:
-        img = np.array(PIL.Image.open(image_path).convert("RGB"))
-        mask = np.array(PIL.Image.open(mask_path).convert("1"), dtype=np.float32)
+    # def visulaize_shade(self, image_path, mask_path, inferred_mask_path, index)->None:
+    #     img = np.array(PIL.Image.open(image_path).convert("RGB"))
+    #     mask = np.array(PIL.Image.open(mask_path).convert("1"), dtype=np.float32)
         
-        inferred_mask = np.array(PIL.Image.open(inferred_mask_path).convert("1"), dtype=np.float32)
+    #     inferred_mask = np.array(PIL.Image.open(inferred_mask_path).convert("1"), dtype=np.float32)
         
-        # Coloring the inferred mask with green
-        inferrred_mask_colored = np.zeros((inferred_mask.shape[0], inferred_mask.shape[1],3), dtype=np.uint8)
-        non_black_index = np.where(inferred_mask == 1)
+    #     # Coloring the inferred mask with green
+    #     inferrred_mask_colored = np.zeros((inferred_mask.shape[0], inferred_mask.shape[1],3), dtype=np.uint8)
+    #     non_black_index = np.where(inferred_mask == 1)
     
-        # CV2 uses the BGR color so inferrred_mask_image would be green, you may print and check
-        inferrred_mask_colored[non_black_index[0], non_black_index[1], :] = (0, 255, 0)
-        first_segmentation_overlay = cv2.addWeighted(img, 0.5, inferrred_mask_colored, 0.5, 0)
+    #     # CV2 uses the BGR color so inferrred_mask_image would be green, you may print and check
+    #     inferrred_mask_colored[non_black_index[0], non_black_index[1], :] = (0, 255, 0)
+    #     first_segmentation_overlay = cv2.addWeighted(img, 0.5, inferrred_mask_colored, 0.5, 0)
 
-        # Coloring the original mask with Blue
-        orig_mask_image = np.zeros((mask.shape[0] ,mask.shape[1],3), dtype=np.uint8)
-        non_black_index = np.where(mask == 1)
-        orig_mask_image[non_black_index[0], non_black_index[1], :] = (255, 0, 0)
-        sec_segmentation_overlay = cv2.addWeighted(first_segmentation_overlay, 0.65, orig_mask_image, 0.35, 0)
+    #     # Coloring the original mask with Blue
+    #     orig_mask_image = np.zeros((mask.shape[0] ,mask.shape[1],3), dtype=np.uint8)
+    #     non_black_index = np.where(mask == 1)
+    #     orig_mask_image[non_black_index[0], non_black_index[1], :] = (255, 0, 0)
+    #     sec_segmentation_overlay = cv2.addWeighted(first_segmentation_overlay, 0.65, orig_mask_image, 0.35, 0)
         
-        # save the final overlayed image
-        shade_visulaize_apth = f"{REAL_PATH(self.out_folder)}/img_{index}_shade_visulaize.jpg"
+    #     # save the final overlayed image
+    #     shade_visulaize_apth = f"{REAL_PATH(self.out_folder)}/img_{index}_shade_visulaize.jpg"
         
-        cv2.imwrite(shade_visulaize_apth,sec_segmentation_overlay)
+    #     cv2.imwrite(shade_visulaize_apth,sec_segmentation_overlay)
 
     def visulaize_sharp(self, image_path, mask_path, inferred_mask_path, index)->None:
         
         img = np.array(PIL.Image.open(image_path).convert("RGB"))
         mask = np.array(PIL.Image.open(mask_path).convert("1"), dtype=np.float32)        
         inferred_mask = np.array(PIL.Image.open(inferred_mask_path).convert("L"), dtype=np.float32)
-        # print(stam.shape)
-        # amir = np.array(stam)
-        # print(np.count_nonzero(amir == 2))
-        # print(inferred_mask.shape)
-        # inferred_mask=inferred_mask.cpu()
-        # amir = np.array(inferred_mask)
-        # print(np.count_nonzero(amir == 2))
-        # print(inferred_mask.shape)
-        # inferred_mask = inferred_mask.squeeze(dim=0)
-        # inferred_mask = inferred_mask.squeeze(dim=1)
-        # inferred_mask = inferred_mask.squeeze(dim=1)
-        # inferred_mask = amir.reshape(amir.shape[2], amir.shape[3])
 
-
-        # .squeeze(1)
-        # print(inferred_mask.shape)
-
-        # inferred_mask = inferred_mask.cpu()
         bad_pathologist_artifatcs = np.where((inferred_mask == 2))
-        print(bad_pathologist_artifatcs)
         both_non_black_index = np.where((inferred_mask == 1) & (mask == 1))
         infer_non_black_index = np.where((inferred_mask == 1) & (mask == 0))
         mask_non_black_index = np.where((inferred_mask == 0) & (mask == 1))
@@ -146,7 +122,6 @@ class Inferer:
         img[mask_non_black_index[0], mask_non_black_index[1], : ] = (255, 0, 0)
         
         sharp_visulaize_path = f"{REAL_PATH(self.out_folder)}/img_{index}_sharp_visulaize.jpg"
-        # save_012_mask_as_img(inferred_mask, inferred_mask_path)
         
         # #add legend
         # cv2.rectangle(img, (20, 20), (500, 250), (200, 200, 200), -1)
@@ -171,9 +146,6 @@ class Inferer:
         
         # cv2.rectangle(img, (position[0]-40, position[1]-30), (position[0], position[1] + 20), color, -1)
         # cv2.putText(img, text, position, cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 255, 255), 2)
-        
-        
-        
         cv2.imwrite(sharp_visulaize_path,img)
 
     
@@ -226,7 +198,6 @@ if __name__ == "__main__":
     
     out_folder = os.path.join("test_inference/", args.model_name, args.test_dataset)
     # REAL_PATH(out_folder)
-
 
     unet_inferer = Inferer(args.model_name, out_folder=out_folder)
     unet_inferer.infer(dataloader, visulaize=args.visualize)
